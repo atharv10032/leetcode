f0d02f2b2356b47a5904eaa3f1e02359303affb4
kazuma1469 <2022meb1301@iitrpr.ac.in>
2024-06-08 16:08:27 +0530

    Solve Valid Paranthesis

---
+++ valid_paranthesis.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define f(i, n) for (int i = 0; i < n; i++)
+#define vll vector<long long>
+using namespace std;
+
+bool isValid(string s)
+{
+    char point = ' ';
+
+    stack<char> check;
+    for (auto x : s)
+    {
+        if (x == '(' || x == '{' || x == '[')
+        {
+            check.push(x);
+            point = x;
+            continue;
+        }
+        else
+        {
+            if (x == ')' && point == '(')
+                check.pop();
+            else if (x == '}' && point == '{')
+                check.pop();
+            else if (x == ']' && point == '[')
+                check.pop();
+            else
+                return false;
+
+            point = check.top();
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << isValid("()") << endl;
+
+    return 0;
+}
